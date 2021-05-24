d0595ce9a3ed6e3c25c90235ac85dd8753cd84fb
Daniel Ladi <59119926+adanikel@users.noreply.github.com>
2021-05-24 05:36:16 -0700

    Update Binance_Client.cpp

--- src/Binance_Client.cpp
+++ src/Binance_Client.cpp
@@ -5955,7 +5955,6 @@ FuturesClientUSDT::~FuturesClientUSDT()
 void FuturesClientUSDT::v__init_ws_session()
 {
 	this->_ws_client->set_host_port(_WS_BASE_FUTURES_USDT, _WS_PORT_FUTURES);
-	this->_ws_client->set_gzip_conversion(0);
 }
 
 /**
